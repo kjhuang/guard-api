60d0cd7708cc89cc67b07fb3cc7a6593b540730b
Phil Huang <phil_kj_huang@wistron.com>
2024-12-16 14:20:28 +0800

    modify AnnounceView: hide site_id, content_path

--- app/schemas/announce_schema.py
+++ app/schemas/announce_schema.py
@@ -4,7 +4,7 @@ announce schema
 
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, field_serializer, computed_field
+from pydantic import BaseModel, ConfigDict, field_serializer, computed_field, Field
 
 from app.schemas.site_schema import Site
 import app.utils.config as config
@@ -23,13 +23,16 @@ class Announce(BaseModel):
     severity: str
     content_path: str
     publish_date: datetime
-    
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class AnnounceView(Announce):
+    site_id: str = Field(..., exclude=True)
+    content_path: str = Field(..., exclude=True)
+
     site: Site | None = None
+
     @field_serializer("publish_date")
     def format_publish_date(self, value: datetime | None) -> str | None:
         if value is None:
@@ -39,4 +42,4 @@ class AnnounceView(Announce):
     @computed_field
     @property
     def url(self) -> str:
-        return config.BLOB_URL_PREFIX + self.content_path
+        return config.BLOB_URL_PREFIX + self.content_path if self.content_path else None
