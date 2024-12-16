e9d6064ccf02b6fc4a9e2439e5d8b5b7a037bc50
Phil Huang <phil_kj_huang@wistron.com>
2024-12-16 14:33:22 +0800

    change expire days to 180

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -17,7 +17,7 @@ from jose import JWTError, jwt
 # Secret key and algorithm for JWT
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_DAYS = 180
 
 API_KEY_NAME = "WIS-API-KEY"
 API_KEYS = ["123456"]
@@ -36,7 +36,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         expire = datetime.now(timezone.utc) + expires_delta
     else:
         expire = datetime.now(timezone.utc) + timedelta(
-            minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+            days=ACCESS_TOKEN_EXPIRE_DAYS
         )
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
